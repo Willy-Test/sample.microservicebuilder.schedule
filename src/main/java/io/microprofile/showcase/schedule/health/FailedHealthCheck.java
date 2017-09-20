1c892c6fbd24879d8599d770c8aa6e8ebe22a31f
jagraj <jagraj@us.ibm.com>
2017-09-20 19:20:28 -0400

    Fixed Health Check Logic with ConfigAPI

--- src/main/java/io/microprofile/showcase/schedule/health/FailedHealthCheck.java
+++ src/main/java/io/microprofile/showcase/schedule/health/FailedHealthCheck.java
@@ -1,10 +1,16 @@
 package io.microprofile.showcase.schedule.health;
 
-import javax.enterprise.context.ApplicationScoped;
+import java.util.Optional;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+
+import io.microprofile.showcase.schedule.resources.ScheduleResource;
 
 /**
  * 
@@ -14,9 +20,21 @@ import org.eclipse.microprofile.health.HealthCheckResponse;
 @Health
 @ApplicationScoped
 public class FailedHealthCheck implements HealthCheck{
+	
+	@Inject
+	private ScheduleResource scheduleResource;
+	@Inject 
+	@ConfigProperty(name="isAppDown") Optional<String> isAppDown;
+	
     @Override
     public HealthCheckResponse call() {
-        return HealthCheckResponse.named("Schedule:failed-check").down().build();
+		try {
+			if(scheduleResource.nessProbe().getStatus()!=200 || ((isAppDown.isPresent()) && (isAppDown.get().equals("true")))) {
+				return HealthCheckResponse.named("Schedule:failed-check").down().build();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return HealthCheckResponse.named("Schedule:successful-check").up().build();
     }
-
 }
